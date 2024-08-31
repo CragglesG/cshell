830f27d7b5b29174b5f3a30e2bb937e6058decf3
Craig Giles <121510295+CragglesG@users.noreply.github.com>
2024-08-31 13:38:40 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,23 +1,28 @@
 import sys
 
-def ask():
+def ask() -> None:
     # Signal for input
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
+    send("$ ")
 
     msg = input()
     return msg.split(" ")
 
+
+def send(msg: str) -> None:
+    sys.stdout.write(msg)
+    sys.stdout.flush()
+
 def main():
     # Verify if use input is a command
-    while True:
-        msg = ask()
-        if msg[0] == "exit":
+    msg = ask()
+    match msg[0]:
+        case "exit":
             sys.exit(int(msg[1]))
-        else:
-            sys.stdout.write(f"{msg[0]}: command not found\n")
-            sys.stdout.flush()
-            continue
+        case "echo":
+            send(" ".join(msg[1:]) + "\n")
+        case _:
+            send(f"{msg[0]}: command not found\n")
+    main()
 
         
         
