eb7dcbf8ea2f963ff1766fdd29ece41828aa3938
Craig Giles <121510295+CragglesG@users.noreply.github.com>
2024-08-31 13:27:37 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,18 +6,23 @@ def ask():
     sys.stdout.flush()
 
     msg = input()
-    return msg
+    return msg.split(" ")
 
 def main():
-    # List of known commands
-    cmds = []
-
     # Verify if use input is a command
     while True:
         msg = ask()
-        if msg not in cmds:
-            sys.stdout.write(f"{msg}: command not found\n")
+        if msg[0] == "exit":
+            sys.exit(int(msg[1]))
+        else:
+            sys.stdout.write(f"{msg[0]}: command not found\n")
             sys.stdout.flush()
+            continue
+        
+
+
+        
+        
 
 
 if __name__ == "__main__":
